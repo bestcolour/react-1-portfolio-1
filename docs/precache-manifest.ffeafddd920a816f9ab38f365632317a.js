a5262e73ce00c5dd10b48ced13cbe7e494a929d5
bestcolour <49665281+bestcolour@users.noreply.github.com>
2020-09-25 07:47:57 +0800

    Added Docs

---
+++ docs/precache-manifest.ffeafddd920a816f9ab38f365632317a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a4cf5e47713c22ca70a199f0ca8956",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30ba732447ab4bcdf3e6",
+    "url": "/static/css/main.36f3d5ac.chunk.css"
+  },
+  {
+    "revision": "1018a420dd71afb62a36",
+    "url": "/static/js/2.9824504d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9824504d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ba732447ab4bcdf3e6",
+    "url": "/static/js/main.0e7ea099.chunk.js"
+  },
+  {
+    "revision": "52afaf95572a706ffaf4",
+    "url": "/static/js/runtime-main.ff2ccb6b.js"
+  }
+]);
