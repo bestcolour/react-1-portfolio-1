1f69b31326a851c1582a69670ba69d3c7ebd3ce0
bestcolour <49665281+bestcolour@users.noreply.github.com>
2020-09-25 07:57:04 +0800

    2nd try

---
+++ docs/precache-manifest.f197571670dead97bebf8d1dd080bce3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c933719779f512b8c39b91d61cd7e6f0",
+    "url": "/bestcolour-portfolio-unused/index.html"
+  },
+  {
+    "revision": "621829891ebcb3e1ad26",
+    "url": "/bestcolour-portfolio-unused/static/css/main.36f3d5ac.chunk.css"
+  },
+  {
+    "revision": "1018a420dd71afb62a36",
+    "url": "/bestcolour-portfolio-unused/static/js/2.9824504d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bestcolour-portfolio-unused/static/js/2.9824504d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621829891ebcb3e1ad26",
+    "url": "/bestcolour-portfolio-unused/static/js/main.a9bcdd60.chunk.js"
+  },
+  {
+    "revision": "2e8fd4bc1055fc6b1af8",
+    "url": "/bestcolour-portfolio-unused/static/js/runtime-main.ee5d4942.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffeafddd920a816f9ab38f365632317a.js"
+  "/bestcolour-portfolio-unused/precache-manifest.f197571670dead97bebf8d1dd080bce3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bestcolour-portfolio-unused/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
